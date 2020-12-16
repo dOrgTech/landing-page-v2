b014be77c2be373a85df04227ac7fc4cc71f354e
unknown <bennett_noah11@yahoo.com>
2020-12-16 14:54:28 -0500

    start zeplin integration

---
+++ src/components/Button1.tsx
@@ -0,0 +1,18 @@
+import { faTelegram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Grid, Link, styled, useTheme } from '@material-ui/core'
+import React from 'react'
+
+
+
+
+
+export const Footer = () => {
+    const theme = useTheme()
+  
+    return (
+   
+         <h1>test</h1>
+          
+    );
+  }
