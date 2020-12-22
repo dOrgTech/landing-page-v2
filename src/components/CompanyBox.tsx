dc26d9be130f29517b59127f991b320b3c358864
unknown <bennett_noah11@yahoo.com>
2020-12-22 05:46:08 -0500

    add companies to homepage

--- src/components/CompanyBox.tsx
+++ src/components/CompanyBox.tsx
@@ -5,39 +5,39 @@ import { Company } from "../constants/company";
 import { theme } from "../theme";
 
 const StyleBox = styled(Box)({
-  width: '21.25rem',
-  height: '4rem',
-  padding: '1.75rem 1.75rem 1.75rem 1.75rem',
+  width: '100%',
   backgroundColor: 'transparent',
+  height:'100%',
   boxSizing: 'border-box',
+  overflow:'no-wrap',
   '&:hover': {
     backgroundColor: 'rgba(0, 0, 0, 0.15)'
-  }
+    
+  },
+  marginRight:'auto'
 });
 
 const StyleTitle = styled(Typography)({
-  width: '100%',
-  margin: '1.938rem 0 0 0',
-  padding:'.1rem',
+  width: '80%',
   fontStretch: 'normal',
   fontStyle: 'normal',
   letterSpacing: '1.5px',
-  textAlign: 'right',
-  float:'right',
-  color: theme.palette.text.primary
+  textAlign: 'left',
+  float:'left',
+  color: theme.palette.text.primary,
+  
 });
 
 const StyleStat = styled(Typography)({
   width: "100%",
   height: "100%",
   fontFamily: theme.typography.fontFamily,
-  fontSize: "3.25rem",
   fontWeight: "bold",
   fontStretch: "normal",
   fontStyle: "normal",
   lineHeight: 1.35,
   letterSpacing: "normal",
-  textAlign: "center",
+  textAlign: "start",
   color: theme.palette.text.primary
 });
 
@@ -45,7 +45,8 @@ const StyleIcon = styled('img')({
   width: "1.5rem",
   height: "1.5rem",
   objectFit: "contain",
-  float: 'left'
+  float: 'left',
+  margin:'1rem 1rem 0 0'
  
 });
 
@@ -56,12 +57,12 @@ interface Props {
 export const CompanyBox: React.FC<Props> = (props: Props) => {
   return (
     <StyleBox>
-      <Grid container alignItems='flex-start'>
+    
         
-        <Grid container direction='row'>
-          <StyleIcon src={props.company.icon} alt='icon' />
-          <StyleTitle variant = 'h5'>{props.company.name}</StyleTitle>
-        </Grid>
+      <Grid container direction='row'>
+        <StyleTitle variant = 'h5'>{props.company.name} <StyleIcon src={props.company.icon} alt='icon' /></StyleTitle>
+         
+   
         
       </Grid>
     </StyleBox>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,15 @@
 import {
-  Box,
-  Grid,
-  Button,
-  styled
+  Box,styled
 } from "@material-ui/core";
 import ReactGA from "react-ga";
 import React from "react";
-import { CompanyBox } from "../components/CompanyBox";
-import { PitchBox } from "../components/PitchBox";
-import { Company, companies} from "../constants/company";
-import { Pitch, pitches } from "../constants/pitches";
+import {Companycomp} from "../components/Companycomp"
 const Root = styled(Box)({
   margin: 'auto'
 });
 
 
 
-const StatsContainer = styled(Grid)({
-  width: '42.5rem',
-});
-
-const PitchesContainer = styled(Grid)({
-  width: '85rem'
-});
 
 
 
@@ -30,28 +17,14 @@ interface Props {
   onClick?: () => void;
 }
 
-export const Home: React.FC<Props> = (props: Props) => {
+export const Home: React.FC<Props> = (props: Props): JSX.Element => {
 
   ReactGA.pageview('home');
 
-  const [counter, setCounter] = React.useState(0);
 
   return (
     <Root>
-
-
-      <StatsContainer container spacing={0} justify="center">
-        {Object.values(companies).map((company: Company, index: number) => (
-          <Grid item xs={11} key={`company-${index}`} direction="column">
-            <CompanyBox company={company} />
-          </Grid>
-        ))}
-      </StatsContainer>
-     
-     
+      <Companycomp/>    
     </Root>
-
-
-
   );
 };
