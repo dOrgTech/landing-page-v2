8726e39fba7724393e3019883cc39aa30ec4fd08
Kris <kris@dorg.tech>
2021-01-03 13:00:56 -0600

    added conditional render to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route, HashRouter } from "react-router-dom";
-import { Box, ThemeProvider } from "@material-ui/core";
+import {Box, Theme, ThemeProvider, useMediaQuery, useTheme} from "@material-ui/core";
 import { theme } from "./theme";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
@@ -11,32 +11,64 @@ import { Footer } from "./components/Footer";
 
 import "./App.css";
 import {Contact} from "./pages/Contact";
+import {HeaderMobile} from "./components/HeaderMobile";
 
 const App: React.FC = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Box bgcolor="primary.main" width="100%" minHeight='100vh'>
-        <HashRouter>
-          <Header />
-          <Switch >
-            <Route path={routes.home.path} exact>
-              <Home onClick={() => console.log("hey")} />
-            </Route>
-            <Route path={routes.about.path}>
-              <About />
-            </Route>
-            <Route path={routes.careers.path}>
-              <Careers />
-            </Route>
-            <Route path={routes.contact.path}>
-              <Contact />
-            </Route>
-          </Switch>
-          <Footer />
-        </HashRouter>
-      </Box>
-    </ThemeProvider>
-  );
+
+  const theme: Theme = useTheme();
+  const desktop = useMediaQuery(theme.breakpoints.up('lg'));
+
+  if (desktop) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Box bgcolor="primary.main" width="100%" minHeight='100vh'>
+          <HashRouter>
+            <Header/>
+            <Switch>
+              <Route path={routes.home.path} exact>
+                <Home onClick={() => console.log("hey")}/>
+              </Route>
+              <Route path={routes.about.path}>
+                <About/>
+              </Route>
+              <Route path={routes.careers.path}>
+                <Careers/>
+              </Route>
+              <Route path={routes.contact.path}>
+                <Contact/>
+              </Route>
+            </Switch>
+            <Footer/>
+          </HashRouter>
+        </Box>
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <ThemeProvider theme={theme}>
+        <Box bgcolor="primary.main" width="100%" minHeight='100vh'>
+          <HashRouter>
+            <HeaderMobile/>
+            <Switch>
+              <Route path={routes.home.path} exact>
+                <Home onClick={() => console.log("hey")}/>
+              </Route>
+              <Route path={routes.about.path}>
+                <About/>
+              </Route>
+              <Route path={routes.careers.path}>
+                <Careers/>
+              </Route>
+              <Route path={routes.contact.path}>
+                <Contact/>
+              </Route>
+            </Switch>
+            <Footer/>
+          </HashRouter>
+        </Box>
+      </ThemeProvider>
+    );
+  }
 };
 
 export default App;
