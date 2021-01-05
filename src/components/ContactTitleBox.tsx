1e2e11bf6675063eb513f6285badf1e8f4faa3f8
Kris <kris@dorg.tech>
2021-01-04 18:52:08 -0600

    made ContactTitleBox responsive

--- src/components/ContactTitleBox.tsx
+++ src/components/ContactTitleBox.tsx
@@ -6,21 +6,21 @@ import { theme } from "../theme";
 const StyledBox = styled(Box)({
   margin: 'auto',
   width: '100%',
-  height: '57.375rem',
+  height: '57.375vw',
   background: 'transparent',
   boxSizing: 'border-box'
 });
 
 const TextContainer = styled(Box)({
-  padding: '6.75rem 1.75rem',
+  padding: '6.75vw 1.75vw',
   background: 'transparent',
   boxSizing: 'border-box'
 });
 
 const StyledTitle = styled(Typography)({
-  maxWidth: '36rem',
+  maxWidth: '36vw',
   fontFamily: theme.typography.fontFamily,
-  fontSize: '3.25rem',
+  fontSize: '3.25vw',
   fontWeight: 600,
   fontStretch: "normal",
   fontStyle: "normal",
@@ -31,9 +31,9 @@ const StyledTitle = styled(Typography)({
 });
 
 const StyledSubTitle = styled(Typography)({
-  margin: '4.25rem 0 0 0',
+  margin: '4.25vw 0 0 0',
   fontFamily: theme.typography.fontFamily,
-  fontSize: '2rem',
+  fontSize: '2vw',
   fontWeight: 600,
   fontStretch: "normal",
   fontStyle: "normal",
@@ -44,9 +44,9 @@ const StyledSubTitle = styled(Typography)({
 });
 
 const StyledInstructions = styled(Typography)({
-  maxWidth: '28.875rem',
+  maxWidth: '28.875vw',
   fontFamily: theme.typography.fontFamily,
-  fontSize: '1rem',
+  fontSize: '1vw',
   fontWeight: 'normal',
   fontStretch: "normal",
   fontStyle: "normal",
