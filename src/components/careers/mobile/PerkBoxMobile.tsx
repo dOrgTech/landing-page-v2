d7a6acc4bccf37a045d435f0cb90ba6485c66f02
Kris <kris@dorg.tech>
2021-01-20 21:36:39 -0600

    added CareersTitleBoxMobile and PerkBoxMobile components

---
+++ src/components/careers/mobile/PerkBoxMobile.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Box, styled, Typography, Grid } from '@material-ui/core'
+import { theme } from "../../../theme";
+import { Perk } from "../../../constants/perks";
+import {hexToRGB} from "../../../Utils/colorUtils";
+
+const StyledGrid = styled(Grid)({
+  margin: 'auto',
+  width: '100%',
+  height: '58.5vw',
+  padding: '9vw 0 0 5.5vw',
+  backgroundColor: 'rgba(0, 0, 0, 0.15)',
+  boxSizing: 'border-box',
+  '&:hover': {
+    backgroundColor: hexToRGB(theme.palette.secondary.main, 0.15)
+  }
+});
+
+const StyleDetail = styled(Typography)({
+  maxWidth: '74vw',
+  marginTop: '2.85vw',
+  fontFamily: theme.typography.fontFamily,
+  fontSize: '3.33vw',
+  fontWeight: 500,
+  fontStretch: 'normal',
+  fontStyle: 'normal',
+  lineHeight: 2,
+  letterSpacing: '-0.42px',
+  textAlign: 'left',
+  color: theme.palette.text.secondary
+});
+
+const StyledPerk = styled(Typography)({
+  marginTop: '7.5vw',
+  fontFamily: theme.typography.fontFamily,
+  fontSize: '5.7vw',
+  fontWeight: 600,
+  fontStretch: "normal",
+  fontStyle: "normal",
+  lineHeight: 1,
+  letterSpacing: '-0.72px',
+  textAlign: "left",
+  color: theme.palette.text.primary
+});
+
+const StyledIcon = styled('img')({
+  width: "11.43vw",
+  height: 'auto',
+  margin: '0 7.6vw 0 0',
+  objectFit: "contain",
+  float: 'left'
+});
+
+interface Props {
+  perk: Perk;
+  classes?: string;
+}
+
+export const PerkBoxMobile: React.FC<Props> = (props: Props) => {
+  return (
+    <StyledGrid className={props.classes} container direction='column' spacing={0} justify='flex-start' alignItems='flex-start'>
+      <Grid item>
+        <StyledIcon src={props.perk.icon} alt='icon' />
+      </Grid>
+      <Grid item>
+        <StyledPerk>{props.perk.perk}</StyledPerk>
+      </Grid>
+      <Grid item>
+        <StyleDetail>{props.perk.detail}</StyleDetail>
+      </Grid>
+    </StyledGrid>
+  );
+}
--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Grid, makeStyles, styled} from "@material-ui/core";
+import {Grid, makeStyles, styled, Theme, useMediaQuery, useTheme} from "@material-ui/core";
 import { Perk, perks } from "../constants/perks";
 import { PerkBox } from "../components/careers/desktop/PerkBox";
 import {CareersTitleBox} from "../components/careers/desktop/CareersTitleBox";
@@ -15,6 +15,8 @@ import {testimonials} from "../constants/testimonials";
 import {TestimonialSection} from "../components/careers/desktop/TestimonialSection";
 import {CurrentOpeningSection} from "../components/careers/desktop/CurrentOpeningSection";
 import {openings} from "../constants/openings";
+import {CareersTitleBoxMobile} from "../components/careers/mobile/CareersTitleBoxMobile";
+import {PerkBoxMobile} from "../components/careers/mobile/PerkBoxMobile";
 
 
 const Root = styled(Grid)({
@@ -39,44 +41,92 @@ export const Careers: React.FC = () => {
 
   const borders = useBorders();
 
-  return (
-    <Root container spacing={0} direction='row' justify="flex-start" alignItems='flex-start'>
-      <LeftMargin xs={1} border={borderStyle} height='189.625vw'/>
-      <ContentContainer container item xs={10} spacing={0} direction='row' justify="center" alignItems='flex-start'>
-        <Grid item xs={12}>
-          <CareersTitleBox textPrimary={'Discover a new way to'} textSecondary={'work'} classes={borders.bottomLeftBorder}/>
-        </Grid>
-        <PerksContainer container spacing={0} justify="center">
-          {Object.values(perks).map((perk: Perk, index: number) => (
-            <Grid item xs={3} key={`perk-${index}`}>
-              <PerkBox perk={perk} classes={borders.leftBorder} />
-            </Grid>
-          ))}
-        </PerksContainer>
-        <Grid item xs={12}>
-          <ActivationPromptBox prompt={'Get started by completing an activation challenge.'}
-            buttonText={'APPLY NOW'}
-            onButtonClick={navigationToActivation}
-            classes={borders.leftBorder}/>
-        </Grid>
-        <Grid item xs={12}>
-          <MeetBuildersTitleBox text={'Meet the Builders'} classes={borders.leftBorder} />
-        </Grid>
-        <Grid item xs={12}>
-          <ProfileWheel members={testMembers} classes={borders.leftBorder} />
-        </Grid>
-        <Grid item xs={12}>
-          <TestimonialSection testimonials={testimonials} carouselBorder={borderStyle} classes={borders.leftBorder}  />
-        </Grid>
-        <Grid item xs={12}>
-          <CurrentOpeningSection openings={openings}
-            titleText={'Current Openings'}
-            buttonText={'APPLY NOW'}
-            onButtonClick={navigationToActivation}
-            classes={borders.leftBorder}  />
-        </Grid>
-      </ContentContainer>
-      <RightMargin xs={1} border={borderStyle} height='189.625vw' longAccentIndex={3}/>
-    </Root>
-  );
+  const theme: Theme = useTheme();
+  const desktop = useMediaQuery(theme.breakpoints.up('lg'));
+
+  if (desktop) {
+    return (
+      <Root container spacing={0} direction='row' justify="flex-start" alignItems='flex-start'>
+        <LeftMargin xs={1} border={borderStyle} height='189.625vw'/>
+        <ContentContainer container item xs={10} spacing={0} direction='row' justify="center" alignItems='flex-start'>
+          <Grid item xs={12}>
+            <CareersTitleBox textPrimary={'Discover a new way to'} textSecondary={'work'}
+              classes={borders.bottomLeftBorder}/>
+          </Grid>
+          <PerksContainer container spacing={0} justify="center">
+            {Object.values(perks).map((perk: Perk, index: number) => (
+              <Grid item xs={3} key={`perk-${index}`}>
+                <PerkBox perk={perk} classes={borders.leftBorder}/>
+              </Grid>
+            ))}
+          </PerksContainer>
+          <Grid item xs={12}>
+            <ActivationPromptBox prompt={'Get started by completing an activation challenge.'}
+              buttonText={'APPLY NOW'}
+              onButtonClick={navigationToActivation}
+              classes={borders.leftBorder}/>
+          </Grid>
+          <Grid item xs={12}>
+            <MeetBuildersTitleBox text={'Meet the Builders'} classes={borders.leftBorder}/>
+          </Grid>
+          <Grid item xs={12}>
+            <ProfileWheel members={testMembers} classes={borders.leftBorder}/>
+          </Grid>
+          <Grid item xs={12}>
+            <TestimonialSection testimonials={testimonials} carouselBorder={borderStyle} classes={borders.leftBorder}/>
+          </Grid>
+          <Grid item xs={12}>
+            <CurrentOpeningSection openings={openings}
+              titleText={'Current Openings'}
+              buttonText={'APPLY NOW'}
+              onButtonClick={navigationToActivation}
+              classes={borders.leftBorder}/>
+          </Grid>
+        </ContentContainer>
+        <RightMargin xs={1} border={borderStyle} height='189.625vw' longAccentIndex={3}/>
+      </Root>
+    );
+  } else {
+    return (
+      <Root container spacing={0} direction='row' justify="flex-start" alignItems='flex-start'>
+        <LeftMargin xs={1} border={borderStyle} height='189.625vw'/>
+        <ContentContainer container item xs={10} spacing={0} direction='row' justify="center" alignItems='flex-start'>
+          <Grid item xs={12}>
+            <CareersTitleBoxMobile textPrimary={'Discover a new way to'} textSecondary={'work'}
+              classes={borders.bottomLeftBorder}/>
+          </Grid>
+          <PerksContainer container spacing={0} justify="center">
+            {Object.values(perks).map((perk: Perk, index: number) => (
+              <Grid item xs={12} key={`perk-${index}`}>
+                <PerkBoxMobile perk={perk} classes={borders.bottomLeftBorder}/>
+              </Grid>
+            ))}
+          </PerksContainer>
+          <Grid item xs={12}>
+            <ActivationPromptBox prompt={'Get started by completing an activation challenge.'}
+              buttonText={'APPLY NOW'}
+              onButtonClick={navigationToActivation}
+              classes={borders.leftBorder}/>
+          </Grid>
+          <Grid item xs={12}>
+            <MeetBuildersTitleBox text={'Meet the Builders'} classes={borders.leftBorder}/>
+          </Grid>
+          <Grid item xs={12}>
+            <ProfileWheel members={testMembers} classes={borders.leftBorder}/>
+          </Grid>
+          <Grid item xs={12}>
+            <TestimonialSection testimonials={testimonials} carouselBorder={borderStyle} classes={borders.leftBorder}/>
+          </Grid>
+          <Grid item xs={12}>
+            <CurrentOpeningSection openings={openings}
+              titleText={'Current Openings'}
+              buttonText={'APPLY NOW'}
+              onButtonClick={navigationToActivation}
+              classes={borders.leftBorder}/>
+          </Grid>
+        </ContentContainer>
+        <RightMargin xs={1} border={borderStyle} height='189.625vw' longAccentIndex={3}/>
+      </Root>
+    );
+  }
 }
