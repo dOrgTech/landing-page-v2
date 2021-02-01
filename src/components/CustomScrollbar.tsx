3665d68637a79ee1a923282511408bc43fab1333
Kris <kris@dorg.tech>
2021-01-31 19:20:59 -0600

    hid scrollbar on mobile, fixed tab width issue introduced in last commit

--- src/components/CustomScrollbar.tsx
+++ src/components/CustomScrollbar.tsx
@@ -1,29 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import { Scrollbar } from "react-scrollbars-custom";
-import {makeStyles} from "@material-ui/core";
+import {makeStyles, Theme, useMediaQuery, useTheme} from "@material-ui/core";
+import {ScrollState} from "react-scrollbars-custom/dist/types/types";
 
 // see: https://github.com/xobotyi/react-scrollbars-custom
-
-const useScrollbarstyles = makeStyles({
-  thumbY: {
-
-  },
-  trackY: {
-
-  },
-  content: {
-
-  },
-  scroller: {
-
-  },
-  wrapper: {
-
-  },
-  holder: {
-
-  }
-});
 
 interface Props {
   rtl?: boolean;
@@ -34,19 +14,39 @@ interface Props {
 
 export const CustomScrollbar: React.FC<Props> = (props: Props) => {
 
-  const scrollbarStyles = useScrollbarstyles();
+  const theme: Theme = useTheme();
+  const desktop = useMediaQuery(theme.breakpoints.up('lg'));
+
+  const [show, setShow] = useState(false);
+  const handleScrollStart = (scrollState: ScrollState) => { setShow(true); }
+  const handleScrollStop = (scrollState: ScrollState) => { setShow(false); }
+
+  const scrollbarStyles = makeStyles({
+    thumbY: {
+      opacity: desktop ? 1 : (show ? 1 : 0)
+    },
+    trackY: {
+      opacity: desktop ? 1 : (show ? 1 : 0)
+    },
+    content: { },
+    scroller: { },
+    wrapper: { },
+    holder: { }
+  })();
 
   return (
     <Scrollbar
-      style={{...props.style}}
       rtl={props.rtl}
       noScrollX={props.noScrollX}
+      style={{...props.style}}
       thumbYProps={{ className: scrollbarStyles.thumbY }}
       trackYProps={{ className: scrollbarStyles.trackY }}
       contentProps={{ className: scrollbarStyles.content }}
       scrollerProps={{ className: scrollbarStyles.scroller }}
       wrapperProps={{ className: scrollbarStyles.wrapper }}
-      className={scrollbarStyles.holder}>
+      className={scrollbarStyles.holder}
+      onScrollStart={handleScrollStart}
+      onScrollStop={handleScrollStop}>
       {props.children}
     </Scrollbar>
   );
