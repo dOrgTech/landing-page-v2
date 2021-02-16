68e85965dd0f4b914ab3ac785332c2dc5659f099
Kris <kris@dorg.tech>
2021-02-16 11:20:27 -0600

    increased debounced sticky timer and adjusted when in the event flow the click events go off

--- src/components/home/desktop/ClientItem.tsx
+++ src/components/home/desktop/ClientItem.tsx
@@ -51,7 +51,7 @@ export const ClientItem: React.FC<Props> = (props: Props) => {
 
   const classes: string = props.classes ? props.classes : ''
   const isSticky = props.stickyItem === props.client.name;
-  const debouncedIsSticky = useDebounce(isSticky, 50);
+  const debouncedIsSticky = useDebounce(isSticky, 100);
   const [isHover, setIsHover] = useState(false);
   const isHighlight = debouncedIsSticky || isHover;
 
@@ -113,7 +113,7 @@ export const ClientItem: React.FC<Props> = (props: Props) => {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onMouseOver={handleMouseOver}
-      onClick={handleClick}>
+      onClickCapture={handleClick}>
       <Grid item>
         <ClientIcon src={props.client.icon} alt='client icon' className={styles.icon} />
       </Grid>
