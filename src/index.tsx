87593f086acc432eadfaa42dd79370393f994861
Kris <kris@dorg.tech>
2021-02-22 10:20:50 -0600

    adjusted GA initialization to use env variable for GA ID

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import ReactGA from 'react-ga';
 
-ReactGA.initialize('TODO: use dOrg\'s google analytics ID UA-ID');
+const GA_ID: string = process.env.GA_ID || ''
+ReactGA.initialize(GA_ID);
 
 ReactDOM.render(
   <React.StrictMode>
