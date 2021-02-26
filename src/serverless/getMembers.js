0d7b9505e22de1de4161748fdd034ec5a257d0ce
Kris <kris@dorg.tech>
2021-02-25 20:35:27 -0600

    trying members caching with lambda

--- src/serverless/getMembers.js
+++ src/serverless/getMembers.js
@@ -1,8 +1,12 @@
 const {fetchMembers} = require("../server/updateMembers");
 
+let members;
+
 exports.handler = async (event, context, callback) => {
-  const apiKey = process.env.HS_API_KEY;
-  const members = await fetchMembers(apiKey);
+  if (!members) {
+    const apiKey = process.env.HS_API_KEY;
+    members = await fetchMembers(apiKey);
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(members),
