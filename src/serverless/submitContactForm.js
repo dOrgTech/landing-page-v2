39a74d69451819223f71370378ee39845003dc93
krisbitney <kris@dorg.tech>
2021-05-21 20:50:20 -0500

    testing aws/serverless function

--- src/serverless/submitContactForm.js
+++ src/serverless/submitContactForm.js
@@ -2,7 +2,7 @@
 
 exports.handler = async (event, context, callback) => {
   console.log(event.body);
-  const apiKey = process.env.AIRTABLE_API_KEY ?? "";
+  const apiKey = process.env.AIRTABLE_API_KEY;
   return fetch(
     "https://api.airtable.com/v0/app6IBhJWYR4dcak6/Contacts",
     {
