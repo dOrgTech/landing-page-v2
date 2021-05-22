155a4af7deba7015620aa4ec034f8f80199261c8
krisbitney <kris@dorg.tech>
2021-05-21 20:51:35 -0500

    testing aws/serverless function

--- src/serverless/submitContactForm.js
+++ src/serverless/submitContactForm.js
@@ -11,7 +11,7 @@ exports.handler = async (event, context, callback) => {
         "Authorization": `Bearer ${apiKey}`,
         "Content-Type": "application/json"
       }),
-      body: body
+      body: event.body
     }
   )
 }
