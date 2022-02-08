0a53f14c6b005e55682294f229f126f44732141c
Colin Spence <udappia@gmail.com>
2022-02-07 21:53:16 -0700

    Creates multiColor version of the FadeAnimation

--- src/components/HeaderDesktop.tsx
+++ src/components/HeaderDesktop.tsx
@@ -3,11 +3,11 @@ import {styled, AppBar, Grid, Link, makeStyles, Box} from "@material-ui/core";
 import { useHistory, useLocation } from 'react-router-dom';
 import {externalLinks, Route, routes} from "../constants/routes";
 import {getBorderStyle} from "../theme/styles";
-import {theme} from "../theme";
+import {colors, theme} from "../theme";
 import {useNavHoverContext} from "./NavHoverContext";
 
 
-const LOGO_PATH = process.env.PUBLIC_URL + "/imgs/logos/dOrg-logo-white.svg";
+const LOGO_PATH = process.env.PUBLIC_URL + "/imgs/logos/dOrg-logo.svg";
 
 const StyledAppBar = styled(AppBar)({
   height: '5vw',
@@ -112,12 +112,12 @@ export const HeaderDesktop: React.FC<Props> = (props: Props) => {
   const borders = makeStyles(getBorderStyle(borderColor))();
 
   const navTextColor = (navHoverIndex: number, index: number) => {
-    if (navHoverIndex === index) return theme.palette.secondary.main;
+    if (navHoverIndex === index) return Object.values(colors)[index];
     else if (props.textColor) return props.textColor;
     else return theme.palette.text.primary;
   }
   const logoColorFilter = (navHoverIndex: number, index: number) => {
-    if (navHoverIndex === index) return 'brightness(0) saturate(100%) invert(74%) sepia(71%) saturate(6519%) hue-rotate(125deg) brightness(99%) contrast(80%)';
+    if (navHoverIndex === index) return 'brightness(300%) saturate(0%) contrast(100%)';
     else if (props.logoColorFilter) return props.logoColorFilter;
     else return '';
   }
@@ -152,10 +152,10 @@ export const HeaderDesktop: React.FC<Props> = (props: Props) => {
                 </StyledLink>
               </LinkBox>
           ))}
-          {isLocation(routes.about) && <Underline style={{right: '28.25vw'}}/>}
-          {isLocation(routes.careers) && <Underline style={{right: '20.25vw'}}/>}
-          {isLocation(externalLinks.blog) && <Underline style={{right: '12vw'}}/>}
-          {isLocation(routes.contact) && <Underline style={{right: '3.75vw'}}/>}
+          {isLocation(routes.about) && <Underline style={{right: '28.25vw', backgroundColor: Object.values(colors)[1]}}/>}
+          {isLocation(routes.careers) && <Underline style={{right: '20.25vw', backgroundColor: Object.values(colors)[2]}}/>}
+          {isLocation(externalLinks.blog) && <Underline style={{right: '12vw', backgroundColor: Object.values(colors)[3]}}/>}
+          {isLocation(routes.contact) && <Underline style={{right: '3.75vw', backgroundColor: Object.values(colors)[4]}}/>}
         </LinksContainer>}
         {renderRight && <HeaderRightMargin item className={borders.bottomBorder}/>}
       </Grid>
